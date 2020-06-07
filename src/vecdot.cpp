b2ddd311c48b8e64c0ed540da19c0291d218050b
milindasf <milinda@cs.utah.edu>
2020-06-06 21:37:53 -0600

    minor fix for vec dot product

--- src/vecdot.cpp
+++ src/vecdot.cpp
@@ -12,20 +12,33 @@
 #include <chrono>
 #include <ctime>
 
-#define SIZE 1024
+//#define SIZE 1024
 namespace sycl = cl::sycl;
-
 #define VECType double
 
-int main() 
+int main(int argc, char** argv) 
 {
-    std::array<VECType, SIZE> vec_a;
-    std::array<VECType, SIZE> vec_b;
+
+    if(argc < 2)
+    {
+      std::cout<<"Usage: "<<argv[0]<<" vecSz iter"<<std::endl;
+      exit(0);
+    }
+
+    const unsigned int VEC_SIZE = atoi(argv[1]);
+    const unsigned int ITER = atoi(argv[2]);
+    double tick_count=0;
+
+    std::vector<VECType> vec_a;
+    std::vector<VECType> vec_b;
+
+    vec_a.resize(VEC_SIZE);
+    vec_b.resize(VEC_SIZE);
 
     VECType dot_ab_dev  = 0.0;
     VECType dot_ab_host = 0.0;
 
-    for (int i = 0; i<SIZE; ++i)
+    for (int i = 0; i<VEC_SIZE; ++i)
     {
         vec_a[i] = 1.0;
         vec_b[i] = 1.0;
@@ -37,12 +50,19 @@ int main()
     std::cout<<"================================================================================="<<std::endl;
 
     auto t_start = std::chrono::high_resolution_clock::now();
-    for (int i = 0; i<SIZE; ++i)
+    #pragma novector noparallel nounroll
+    for(unsigned int t=0; t < ITER; t++)
+    {
+      //std::cout<<"iter: "<<t<<std::endl;
+      dot_ab_host=0;
+      #pragma novector noparallel nounroll
+      for (int i = 0; i<VEC_SIZE; ++i)
         dot_ab_host = dot_ab_host + (vec_a[i]*vec_b[i]);
 
+    }
     auto t_end = std::chrono::high_resolution_clock::now();
-    std::cout<<"time host (mu s): "<<std::chrono::duration_cast<std::chrono::microseconds>(t_end-t_start).count()<<std::endl;
-    
+    tick_count=(std::chrono::duration_cast<std::chrono::milliseconds>(t_end-t_start).count());
+    std::cout<<"time host (s): "<<(tick_count/((double)1000))<<std::endl;
     std::cout<<"dot(vec_a,vec_b): "<<dot_ab_host<<std::endl;
 
     std::cout<<"================================================================================="<<std::endl;
@@ -77,6 +97,7 @@ int main()
 
         sycl::default_selector gpu;
         sycl::queue d_queue(gpu,asyncHandler);
+        std::cout << "Device: "<< d_queue.get_device().get_info<sycl::info::device::name>()<< std::endl;
 
         auto a_size = sycl::range<1>{vec_a.size()};
         auto b_size = sycl::range<1>{vec_b.size()};
@@ -88,7 +109,13 @@ int main()
         sycl::buffer<VECType, 1>  _vec_b(vec_b.data(), b_size);
         sycl::buffer<VECType, 1>  _dot_ab_dev(&dot_ab_dev, dot_ab_size);
 
-        mkl::blas::dot(d_queue, vec_a.size(), _vec_a, 1, _vec_b, 1, _dot_ab_dev);
+        //for(unsigned int t=0; t < ITER; t++)
+          mkl::blas::dot(d_queue, vec_a.size(), _vec_a, 1, _vec_b, 1, _dot_ab_dev);
+
+        d_queue.wait();
+
+
+        
 
 
 
@@ -99,16 +126,13 @@ int main()
               << "OpenCL status: " << e.get_cl_code() << std::endl;
     }
 
-    std::cout<<"time device kernel execution (mu s): "<<std::chrono::duration_cast<std::chrono::microseconds>(startTimeList[1]-startTimeList[0]).count()<<std::endl;
-
+    //std::cout<<"time device kernel execution (s): "<<std::chrono::duration_cast<std::chrono::microseconds>(startTimeList[1]-startTimeList[0]).count()<<std::endl;
     t_end = std::chrono::high_resolution_clock::now();
-    std::cout<<"time device (mu s): "<<std::chrono::duration_cast<std::chrono::microseconds>(t_end-t_start).count()<<std::endl;
-    
-
+    std::cout<<"time device (s): "<<((std::chrono::duration_cast<std::chrono::seconds>(t_end-t_start).count())/((double)ITER))<<std::endl;
     std::cout<<"dot(vec_a,vec_b): "<<dot_ab_dev<<std::endl;
 
 
-
+   return 0;
 
 
 }
